51c847998adb123ebf645f0232dbc4d0a5395870
Hafeez <izzhafeez@gmail.com>
2023-08-18 23:27:32 +0800

    Edited ./coord.rs

--- src/geometry/coord.rs
+++ src/geometry/coord.rs
@@ -1,7 +1,8 @@
+use std::fmt::{Display, Formatter};
 use serde::{Serialize, Deserialize};
 use crate::geometry::geometry::Geometry;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Coord {
     lat: f32,
     lng: f32,
@@ -15,3 +16,9 @@ impl Coord {
 }
 
 impl Geometry for Coord {}
+
+impl Display for Coord {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {}, {})", self.lat, self.lng, self.alt)
+    }
+}
