6e24267f39c766b6f3e352c65ff86aea2839f2a1
Hafeez <izzhafeez@gmail.com>
2023-08-18 23:27:09 +0800

    Edited ./subzone_kml_reader.rs

---
+++ src/io/reader/kml_reader/subzone_kml_reader.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+use regex::Regex;
+use crate::feature::kml::Kml;
+use crate::feature::subzone::subzone::Subzone;
+use crate::io::reader::kml_reader::feature::subzone_reader::SubzoneReader;
+use crate::io::reader::kml_reader::kml_reader::KmlReader;
+use crate::io::reader::reader::Reader;
+
+pub struct SubzoneKmlReader {}
+
+impl KmlReader<Kml> for SubzoneKmlReader {
+    fn get_pattern() -> Regex {
+        Regex::new(r"<Placemark id=(?P<data>.*?)</Placemark>").expect("Invalid regex given.")
+    }
+}
+
+impl Reader<Kml> for SubzoneKmlReader {
+    fn read(string: &str) -> Kml {
+        let reg_exp: Regex = SubzoneKmlReader::get_pattern();
+        let subzones_str: Vec<&str> = reg_exp
+            .captures_iter(string)
+            .map(|capture| {
+                capture.name("data").unwrap().as_str()
+            })
+            .collect();
+        println!("{:?}", reg_exp.captures(string));
+
+        let subzones: HashMap<String, Subzone> = subzones_str
+            .into_iter()
+            .map(SubzoneReader::read)
+            .map(|subzone| (subzone.get_header(), subzone))
+            .collect();
+
+        Kml::new(subzones)
+    }
+}
