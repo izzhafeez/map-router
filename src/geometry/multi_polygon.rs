22213c2b5d0aaf1b4639bbe40ba82350d8e2985c
Hafeez <izzhafeez@gmail.com>
2023-08-18 23:27:32 +0800

    Edited ./multi_polygon.rs

--- src/geometry/multi_polygon.rs
+++ src/geometry/multi_polygon.rs
@@ -1,15 +1,30 @@
+use std::fmt::{Display, Formatter};
 use serde::{Serialize, Deserialize};
 use crate::geometry::geometry::Geometry;
+use crate::geometry::polygon::Polygon;
+use crate::geometry::shape::Shape;
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct MultiGeometry<T: Geometry> {
-    geometries: Vec<Box<T>>
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct MultiPolygon {
+    polygons: Vec<Polygon>
 }
 
-impl<T> MultiGeometry<T> {
-    pub fn from_geometries(geometries: Vec<T>) -> Self {
-        Self { geometries }
+impl MultiPolygon {
+    pub fn from_polygons(polygons: Vec<Polygon>) -> Self {
+        Self { polygons: polygons }
     }
 }
 
-impl Geometry for MultiGeometry<T> {}
+impl Geometry for MultiPolygon {}
+
+impl Shape for MultiPolygon {
+    fn is_in(&self) {
+        todo!()
+    }
+}
+
+impl Display for MultiPolygon {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({:?})", self.polygons)
+    }
+}
