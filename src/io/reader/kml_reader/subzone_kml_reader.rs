89f31a36a514d3bbdf61d83f92d5696e1f156958
Hafeez <izzhafeez@gmail.com>
2023-08-20 14:50:54 +0800

    Edited ./subzone_kml_reader.rs

--- src/io/reader/kml_reader/subzone_kml_reader.rs
+++ src/io/reader/kml_reader/subzone_kml_reader.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 use regex::Regex;
 use crate::feature::kml::Kml;
-use crate::feature::subzone::subzone::Subzone;
-use crate::io::reader::kml_reader::feature::subzone_reader::SubzoneReader;
+use crate::feature::shape::region::Region;
+use crate::feature::shape::subzone::Subzone;
+use crate::io::reader::kml_reader::feature::region_reader::RegionReader;
 use crate::io::reader::kml_reader::kml_reader::KmlReader;
 use crate::io::reader::reader::Reader;
 
@@ -17,19 +18,21 @@ impl KmlReader<Kml> for SubzoneKmlReader {
 impl Reader<Kml> for SubzoneKmlReader {
     fn read(string: &str) -> Kml {
         let reg_exp: Regex = SubzoneKmlReader::get_pattern();
-        let subzones_str: Vec<&str> = reg_exp
+        let regions_str: Vec<&str> = reg_exp
             .captures_iter(string)
             .map(|capture| {
                 capture.name("data").unwrap().as_str()
             })
             .collect();
 
-        let subzones: HashMap<String, Subzone> = subzones_str
-            .into_iter()
-            .map(SubzoneReader::read)
-            .map(|subzone| (subzone.get_header(), subzone))
-            .collect();
+        let mut regions: HashMap<String, Region> = HashMap::new();
+        for (code, r) in regions_str.into_iter().map(RegionReader::read) {
+            match regions.get_mut(&code) {
+                Some(region) => region.combine_with(r),
+                None => { regions.insert(code, r); }
+            }
+        }
 
-        Kml::new(subzones)
+        Kml::new(regions)
     }
 }
