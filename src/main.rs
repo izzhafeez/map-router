4ad4aa2065245aab93b6ef0cdae58cd6e0a22352
Hafeez <izzhafeez@gmail.com>
2023-08-20 14:52:37 +0800

    Edited ./main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ extern crate core;
 use crate::feature::kml::Kml;
 use crate::io::reader::reader::Reader;
 use crate::io::reader::file_reader::file_reader::FileReader;
+use crate::io::reader::json_reader::subzone_json_reader::SubzoneJsonReader;
 use crate::io::reader::kml_reader::subzone_kml_reader::SubzoneKmlReader;
+use crate::io::writer::json_writer::subzone_json_writer::SubzoneJsonWriter;
+use crate::io::writer::writer::Writer;
 
 mod router;
 mod feature;
@@ -11,6 +14,6 @@ mod io;
 mod geometry;
 
 fn main() {
-    let text: String = FileReader::read("./src/data/master-plan-2019-subzone-boundary-no-sea-kml.kml");
-    let subzones: Kml = SubzoneKmlReader::read(&text.replace("\n", ""));
+    let kml: Kml = SubzoneJsonReader::read(&SubzoneJsonReader::get_path());
+    println!("{:?}", kml);
 }
